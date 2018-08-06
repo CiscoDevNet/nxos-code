da520fb6e74a5250518c37299ad1160196d281dd
hpreston <hank.preston@gmail.com>
2018-08-06 10:40:14 -0400

    added intro-nxapi labs

---
+++ intro-nxapi/get_sys_info_no_filter.py
@@ -0,0 +1,30 @@
+import requests
+
+
+# Assign requests.Session instance to session variable
+session = requests.Session()
+
+# Define URL and PAYLOAD variables
+URL = "http://sbx-nxos-mgmt.cisco.com/api/aaaLogin.json"
+PAYLOAD = {
+          "aaaUser": {
+            "attributes": {
+              "name": "admin",
+              "pwd": "Admin_1234!"
+               }
+            }
+          }
+
+# Obtain an authentication cookie
+session.post(URL,json=PAYLOAD)
+
+# Define SYS_URL variable
+SYS_URL = "http://sbx-nxos-mgmt.cisco.com/api/mo/sys.json"
+
+# Obtain system information by making session.get call
+# then convert it to JSON format then filter to system attributes
+sys_info = session.get(SYS_URL).json()
+
+# Print raw data
+
+print(sys_info)
