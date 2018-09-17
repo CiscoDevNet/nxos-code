b30b21b5af1fddeddaf6f58ff49ea073cd5256b3
hpreston <hank.preston@gmail.com>
2018-09-17 15:59:37 -0400

    update for HTTPS

--- intro-nxapi/get_sys_info_no_filter.py
+++ intro-nxapi/get_sys_info_no_filter.py
@@ -1,11 +1,13 @@
-import requests
+import requests, urllib3
 
+# Disable Self-Signed Cert warning for demo
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Assign requests.Session instance to session variable
 session = requests.Session()
 
 # Define URL and PAYLOAD variables
-URL = "http://sbx-nxos-mgmt.cisco.com/api/aaaLogin.json"
+URL = "https://sbx-nxos-mgmt.cisco.com/api/aaaLogin.json"
 PAYLOAD = {
           "aaaUser": {
             "attributes": {
@@ -16,14 +18,14 @@ PAYLOAD = {
           }
 
 # Obtain an authentication cookie
-session.post(URL,json=PAYLOAD)
+session.post(URL,json=PAYLOAD,verify=False)
 
 # Define SYS_URL variable
 SYS_URL = "http://sbx-nxos-mgmt.cisco.com/api/mo/sys.json"
 
 # Obtain system information by making session.get call
 # then convert it to JSON format then filter to system attributes
-sys_info = session.get(SYS_URL).json()
+sys_info = session.get(SYS_URL,verify=False).json()
 
 # Print raw data
 
